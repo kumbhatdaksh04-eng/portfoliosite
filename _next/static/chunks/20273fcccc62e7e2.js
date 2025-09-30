4bd9b5ed1245e19674ab108fe64dac04115e34b9
kumbhatdaksh04-eng <kumbhatdaksh04@gmail.com>
2025-09-30 17:48:18 +0530

    Add files via upload

---
+++ _next/static/chunks/20273fcccc62e7e2.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_error", [
+  "static/chunks/17722e3ac4e00587.js",
+  "static/chunks/f195f10a236dcc21.js",
+  "static/chunks/turbopack-d99abc857ef26ea4.js"
+])
---
+++ _next/static/chunks/6913bbee662ed93d.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/e60ef129113f6e24.js",
+  "static/chunks/f195f10a236dcc21.js",
+  "static/chunks/turbopack-a97d51c07d3fcf18.js"
+])
